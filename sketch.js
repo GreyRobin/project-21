77e12488c664177b9bfe79db7b0bac01c4f28050
GreyRobin <86514645+GreyRobin@users.noreply.github.com>
2021-08-03 17:31:33 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,27 +4,29 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
+var ball,ground,left,right
+var world
+var radius = 40
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
 	var ball_options={
 		isStatic:false,
 		restitution:0.3,
 		friction:0,
 		density:1.2
 	}
-}
-
-function setup() {
-	createCanvas(800, 700);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
 	//Create the Bodies Here.
   ball = Bodies.circle(150,100,20,ball_options)
   World.add(world,ball)
   ellipseMode(RADIUS);
+  ground = new Ground(width/2,670,width,20)
+  left = new Ground(1100,600,20,120)
+  right = new Ground(1350,600,20,120)
 
 	Engine.run(engine);
   
@@ -34,14 +36,20 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  if(keyDown("UP_ARROW")){
-	  force()
-  }
+ellipse(ball.position.x,ball.position.y,20)
+ground.display()
+left.display()
+right.display()
   drawSprites();
  
 }
 
 
 
-function force(){
-  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0.05})
+function keyPressed(){
+	if(keyCode==UP_ARROW){
+
+	
+  Matter.Body.applyForce(ball,ball.position,{x:85,y:-85})
+}
+}
